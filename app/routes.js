b4999d50ec10d8f1faac8c310d9246d546dc63a9
Frankie Roberto <frankie@frankieroberto.com>
2024-05-20 15:13:05 +0100

    NHS regions interface (#8)
    This is an initial sketch of what an interface for the NHS regions might look like.
    
    They’d be able to invite NHS trusts to begin setting up their accounts.
    
    <img width="1209" alt="Screenshot 2024-05-20 at 15 10 49" src="https://github.com/NHSDigital/record-a-vaccination-prototype/assets/30665/4f87b2d4-9b1c-4ada-96b5-165387a8ffc5">

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,40 @@ const express = require('express');
 
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
+
+// Inviting an organisation
+router.post('/regions/v1/add', (req, res) => {
+
+  req.session.data.organisationsAdded ||= [];
+
+  const organisationCode = req.session.data.organisationCode
+  // Add organisation to `organisationsAdded` array
+  req.session.data.organisationsAdded.push({
+    code: req.session.data.organisationCode,
+    name: req.session.data.nhsTrusts[req.session.data.organisationCode],
+    leadUsers: [
+      {email: req.session.data.email}
+    ]
+  })
+
+  // Remove data from adding organisation flow
+  req.session.data.email = '';
+  req.session.data.organisationCode = '';
+
+  res.redirect('/regions/v1/organisations/' + organisationCode);
+
+});
+
+// Viewing an organisation
+router.get('/regions/v1/organisations/:code', (req, res) => {
+
+  const organisationsAdded = req.session.data.organisationsAdded || []
+  const organisation = organisationsAdded.find((org) => org.code === req.params.code)
+
+  res.render('regions/v1/organisation', {
+    organisation
+  });
+
+});
 
 module.exports = router;
