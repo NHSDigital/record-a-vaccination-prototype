8cf4c9ee6e3e83dbc8834d18c2d896f5f071a7b9
Frankie Roberto <frankie@frankieroberto.com>
2024-06-06 13:56:11 +0100

    Add ability to add invite multiple lead users (#13)

--- app/routes.js
+++ app/routes.js
@@ -27,6 +27,21 @@ router.post('/regions/v1/add', (req, res) => {
 
 });
 
+// Inviting a second lead user for an organisation
+router.post('/regions/v1/organisations/:code/add', (req, res) => {
+
+  const organisationsAdded = req.session.data.organisationsAdded || []
+  const organisation = organisationsAdded.find((org) => org.code === req.params.code)
+
+  organisation.leadUsers.push({ email: req.session.data.email })
+
+  // Remove data from adding organisation flow
+  req.session.data.email = '';
+
+  res.redirect('/regions/v1/organisations/' + organisation.code);
+
+});
+
 // Viewing an organisation
 router.get('/regions/v1/organisations/:code', (req, res) => {
 
@@ -34,6 +49,30 @@ router.get('/regions/v1/organisations/:code', (req, res) => {
   const organisation = organisationsAdded.find((org) => org.code === req.params.code)
 
   res.render('regions/v1/organisation', {
+    organisation
+  });
+
+});
+
+// Add another lead user to an organisation form
+router.get('/regions/v1/organisations/:code/add-email', (req, res) => {
+
+  const organisationsAdded = req.session.data.organisationsAdded || []
+  const organisation = organisationsAdded.find((org) => org.code === req.params.code)
+
+  res.render('regions/v1/add-another-email', {
+    organisation
+  });
+
+});
+
+// Check a second lead user for an organisation
+router.get('/regions/v1/organisations/:code/add-email-check', (req, res) => {
+
+  const organisationsAdded = req.session.data.organisationsAdded || []
+  const organisation = organisationsAdded.find((org) => org.code === req.params.code)
+
+  res.render('regions/v1/add-another-email-check', {
     organisation
   });
 
