8c4597c10136e7afcb31c0d92fea7a3ab7fe292d
Frankie Roberto <frankie@frankieroberto.com>
2024-06-12 17:34:41 +0100

    User admin and onboarding flows (#15)

--- app/routes.js
+++ app/routes.js
@@ -78,4 +78,67 @@ router.get('/regions/v1/organisations/:code/add-email-check', (req, res) => {
 
 });
 
+
+// USER ADMINF
+
+// Adding a user
+router.post('/user-admin/v1/add', (req, res) => {
+
+
+  req.session.data.users.push({
+    firstName: req.session.data.firstName,
+    lastName: req.session.data.lastName,
+    email: req.session.data.email,
+    role: req.session.data.role,
+    status: 'Invited'
+  })
+
+  // Reset data
+  req.session.data.email = '';
+  req.session.data.firstName = '';
+  req.session.data.lastName = '';
+  req.session.data.role = '';
+
+  res.redirect('/user-admin/v1');
+
+});
+
+
+// Answering whether the user has a Care Identity account or not
+router.post('/user-onboarding/v1/cis2-answer', (req, res) => {
+
+  const answer = req.body.existingCareIdentityAccount;
+
+  if (answer === 'yes') {
+    res.redirect('/user-onboarding/v1/cis2-setup');
+  } else if (answer === 'no') {
+    res.redirect('/user-onboarding/v1/cis2-alternative');
+  } else if (answer === 'not-sure') {
+    res.redirect('/user-onboarding/v1/cis2-not-sure');
+  } else {
+    res.redirect('/user-onboarding/v1/setup');
+  }
+
+});
+
+// Answering type of authentication method
+router.post('/user-onboarding/v1/cis2-method-answer', (req, res) => {
+
+  const answer = req.body.cis2Method;
+
+  if (answer === 'smartcard') {
+    res.redirect('/user-onboarding/v1/cis2-smartcard');
+  } else if (answer === 'windowsHello') {
+    res.redirect('/user-onboarding/v1/cis2-windows-hello');
+  } else if (answer === 'securityKey') {
+    res.redirect('/user-onboarding/v1/cis2-security-key');
+  }  else if (answer === 'ipadapp') {
+    res.redirect('/user-onboarding/v1/cis2-ipad-app');
+  } else {
+    res.redirect('/user-onboarding/v1/cis2-auth-method');
+  }
+
+});
+
+
 module.exports = router;
