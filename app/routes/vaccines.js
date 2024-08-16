94dd6b6da8c0f8a3e2c945804ba042ee35b51769
Frankie Roberto <frankie@frankieroberto.com>
2024-08-16 11:16:15 +0100

    Fix date bug

--- app/routes/vaccines.js
+++ app/routes/vaccines.js
@@ -7,7 +7,7 @@ module.exports = (router) => {
 
     const generatedId = Math.floor(Math.random() * 10000000).toString()
 
-    const expiryDate = new Date(data['batchExpiryDate-year'], (data['batchExpiryDate-month'] - 1), data['batchExpiryDate-day']).toISOString().substring(0,10)
+    const expiryDate = new Date(data['batchExpiryDate-year'], (data['batchExpiryDate-month'] - 1), data['batchExpiryDate-day'], 12).toISOString().substring(0,10)
 
     req.session.data.vaccines.push({
       id: generatedId,
@@ -68,7 +68,7 @@ module.exports = (router) => {
 
     const generatedId = Math.floor(Math.random() * 10000000).toString()
 
-    const expiryDate = new Date(data['batchExpiryDate-year'], (data['batchExpiryDate-month'] - 1), data['batchExpiryDate-day']).toISOString().substring(0,10)
+    const expiryDate = new Date(data['batchExpiryDate-year'], (data['batchExpiryDate-month'] - 1), data['batchExpiryDate-day'], 12).toISOString().substring(0,10)
 
     vaccine.batches.push({
       id: generatedId,
