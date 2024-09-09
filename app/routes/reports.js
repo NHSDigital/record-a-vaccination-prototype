5a8225a7b2fa813b0f5a22e25cb1a272f24950b0
Frankie Roberto <frankie@frankieroberto.com>
2024-09-09 15:55:10 +0100

    Set the right dates

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -1,3 +1,5 @@
+const filters = require('.././filters.js')()
+
 module.exports = (router) => {
 
   router.get('/reports/choose-dates-2', (req, res) => {
@@ -81,6 +83,55 @@ module.exports = (router) => {
     } else {
       res.redirect('/reports/choose-dates-2?showErrors=true')
     }
+
+  })
+
+
+  router.get('/reports/check', (req, res) => {
+
+    const data = req.session.data
+    const today = new Date()
+    const days = 86400000 // number of milliseconds in a day
+
+    const fromInput = data.from
+    const toInput = data.to
+    const dateOption = data.date
+
+    let from, to
+
+    console.log(dateOption)
+
+    switch (dateOption) {
+      case 'custom_date_range':
+        from = filters.isoDateFromDateInput(fromInput)
+        to = filters.isoDateFromDateInput(toInput)
+        break
+      case 'Today':
+        from = today.toISOString().substring(0,10)
+        to = today.toISOString().substring(0,10)
+        break
+      case 'Yesterday':
+        from = new Date(today.getTime() - (1 * days)).toISOString().substring(0,10)
+        to = new Date(today.getTime() - (1 * days)).toISOString().substring(0,10)
+        break
+      case 'Last7days':
+        from = new Date(today.getTime() - (7 * days)).toISOString().substring(0,10)
+        to = today.toISOString().substring(0,10)
+        break
+      case 'Last14days':
+        from = new Date(today.getTime() - (14 * days)).toISOString().substring(0,10)
+        to = today.toISOString().substring(0,10)
+        break
+      case 'Last30days':
+        from = new Date(today.getTime() - (14 * days)).toISOString().substring(0,10)
+        to = today.toISOString().substring(0,10)
+        break
+    }
+
+    res.render('reports/check', {
+      from,
+      to
+    })
 
   })
 
