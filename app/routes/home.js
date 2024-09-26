9a2e670ca1b2c00813f30375c4d85dc9f1e492cd
Frankie Roberto <frankie@frankieroberto.com>
2024-09-26 13:27:41 +0100

    Add site-specific dashboard

--- app/routes/home.js
+++ app/routes/home.js
@@ -42,4 +42,51 @@ module.exports = router => {
       stats
     })
   })
+
+  // Site-specific dashboard
+  router.get('/home/:siteId', (req, res) => {
+
+    const siteId = req.params.siteId
+    const site = req.session.data.sites[siteId]
+    const dateToday = new Date()
+    const stats = []
+
+    let vaccines = ["RSV", "Pertussis"]
+
+    if (Math.random() > 0.5) { vaccines.push("Flu") }
+    if (Math.random() > 0.5) { vaccines.push("Covid") }
+
+    for (vaccine of vaccines) {
+
+      let vaccineStat = {
+        vaccine: vaccine,
+        days: []
+      }
+
+      for (let i = 0; i < 8; i++) {
+
+        let day = new Date(dateToday)
+        day.setDate(day.getDate() - i)
+
+        let count = Math.round(Math.random() * 5) + 1
+        if (i > 0) {
+          count += Math.round(Math.random() * 30)
+        }
+
+        vaccineStat.days.push({
+          date: day.toISOString().substring(0,10),
+          count: count
+        })
+      }
+
+      stats.push(vaccineStat)
+    }
+
+
+    res.render('home/site', {
+      site,
+      siteId,
+      stats
+    })
+  })
 }
