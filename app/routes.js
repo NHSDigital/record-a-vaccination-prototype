ea048f607f875cebb084e825f1111a81adaeaf86
johnrimmer2 <john.rimmer2@nhs.net>
2025-02-04 17:10:54 +0000

    Add 'Find a record' section (#181)
    This pull request includes a new 'Fidn a record' section within RAVS
    which allows users to find records in order to be able to do the
    following to patient records created in RAVS:
    
    - View
    - Edit
    - Delete
    
    ---------
    
    Co-authored-by: Frankie Roberto <frankie.roberto1@nhs.net>
    Co-authored-by: Frankie Roberto <frankie@frankieroberto.com>

--- app/routes.js
+++ app/routes.js
@@ -10,10 +10,12 @@ require('./routes/user-onboarding')(router)
 require('./routes/user-profile')(router)
 require('./routes/vaccines')(router)
 require('./routes/reports')(router)
+require('./routes/find-record')(router)
 require('./routes/prototype-admin')(router)
 require('./routes/support')(router)
 require('./routes/auth')(router)
 require('./routes/home')(router)
+
 
 
 module.exports = router;
