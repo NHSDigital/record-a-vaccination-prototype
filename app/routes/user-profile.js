12b9b1e7402145e42ad22228d75eb8af73c8b8f8
Frankie Roberto <frankie.roberto1@nhs.net>
2025-04-01 22:35:42 +0100

    Remove user profile v1 (#282)
    This was an older iteration we no longer need.

--- app/routes/user-profile.js
+++ app/routes/user-profile.js
@@ -1,17 +1,17 @@
 module.exports = (router) => {
 
-  router.get('/user-profile/v2', (req, res) => {
+  router.get('/user-profile', (req, res) => {
     const data = req.session.data
     const leadAdminUsersAtSameOrganisation = data.users.filter((user) => (user.organisations || []).find((organisation) => (organisation.id === data.currentOrganisationId && organisation.permissionLevel == 'Lead administrator')))
 
-    res.render('user-profile/v2/index', {
+    res.render('user-profile/index', {
       leadAdminUsersAtSameOrganisation
     })
   })
 
 
   // Updating user profile
-  router.post('/user-profile/v1/update', (req, res) => {
+  router.post('/user-profile/update', (req, res) => {
     const currentUser = req.session.data.users.find((user) => user.id === req.session.data.currentUserId)
 
     if (req.body.firstName) {
@@ -22,37 +22,6 @@ module.exports = (router) => {
       currentUser.lastName = req.body.lastName
     }
 
-    if (req.body.email) {
-      currentUser.lastName = req.body.email
-    }
-
-    if (req.body.professionalBody) {
-      currentUser.professionalBody = req.body.professionalBody
-    }
-
-    if (req.body.doctorNumber) {
-      currentUser.professionalBodyNumber = req.body.doctorNumber
-    }
-
-    if (req.body.nurseNumber) {
-      currentUser.professionalBodyNumber = req.body.nurseNumber
-    }
-
-    res.redirect('/user-profile/v1')
-  })
-
-  // Updating user profile v2
-  router.post('/user-profile/v2/update', (req, res) => {
-    const currentUser = req.session.data.users.find((user) => user.id === req.session.data.currentUserId)
-
-    if (req.body.firstName) {
-      currentUser.firstName = req.body.firstName
-    }
-
-    if (req.body.lastName) {
-      currentUser.lastName = req.body.lastName
-    }
-
-    res.redirect('/user-profile/v2')
+    res.redirect('/user-profile')
   })
 }
