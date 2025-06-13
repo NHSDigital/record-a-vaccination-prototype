5169ddddeb953991f31a3c463d07cdb58722c187
Frankie Roberto <frankie.roberto1@nhs.net>
2025-06-13 15:02:10 +0100

    Add error messages to apply journey (#350)
    This adds some error messages to the pharmacy select page:
    
    Pharmacy already onboarded:
    
    ![Screenshot 2025-06-13 at 14 19
    49](https://github.com/user-attachments/assets/0118b2da-b71e-438a-8473-210a8186c9eb)
    
    
    Pharmacy not selected:
    
    ![Screenshot 2025-06-13 at 14 11
    39](https://github.com/user-attachments/assets/ef316fe7-3313-4aa8-b42f-4f915f0fda15)

--- app/routes/apply.js
+++ app/routes/apply.js
@@ -1,5 +1,47 @@
 module.exports = router => {
 
+  router.get('/apply/start', (req, res) => {
+    let errors = []
+
+    const error = req.query.error
+
+    if (error === "no-pharmacy") {
+      errors.push({
+        text: "Select pharmacy",
+        href: "#organisation-code"
+      })
+    } else if (error === "existing-account") {
+      errors.push({
+        text: "This pharmacy already has access to the service.",
+        href: "#organisation-code"
+      })
+    }
+
+    res.render('apply/start', {
+      errors
+    })
+  })
+
+
+  router.post('/apply/answer-pharmacy', (req, res) => {
+    const data = req.session.data
+    const organisationCode = data.organisationCode
+    const organisation = data.organisations.find((organisation) => organisation.id === organisationCode)
+
+    if (!organisation) {
+      res.redirect('/apply/start?error=no-pharmacy');
+    } else if (organisation.id === "FA424") {
+      res.redirect('/apply/start?error=existing-account');
+    } else {
+      res.redirect('/apply/check-pharmacy')
+    }
+
+    res.render('apply/start', {
+      errors
+    })
+
+  })
+
 
   // Show organisation check page
   router.get('/apply/check-pharmacy', (req, res) => {
