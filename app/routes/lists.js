4391da63679d792eec98c55a1ce2ff7037724020
Frankie Roberto <frankie.roberto1@nhs.net>
2025-07-07 13:18:21 +0100

    Update first page of Lists (#388)
    This updates the first page of the Lists feature with 2 states:
    
    * an empty state containing no lists and a How it works section
    * where there are 2 lists, and "How it works" is collapsed into a
    Details component
    
    
    ## Screenshots
    
    ### Empty state
    
    ![Screenshot 2025-07-07 at 13 03
    10](https://github.com/user-attachments/assets/6a4e267b-e391-45d0-8e81-68dc2942fff6)
    
    
    ### State with some lists
    
    ![Screenshot 2025-07-07 at 13 03
    47](https://github.com/user-attachments/assets/37821e77-e978-4702-b5fc-21e77d07f137)

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -1,5 +1,29 @@
 module.exports = router => {
 
 
+  router.get('/lists', (req, res) => {
+
+    const currentOrganisation = res.locals.currentOrganisation;
+    const lists = currentOrganisation.lists || []
+
+    res.render('lists/index', {
+      lists
+    })
+
+  })
+
+  router.post('/lists/add-numbers', (req, res) => {
+
+    const currentOrganisation = res.locals.currentOrganisation;
+    currentOrganisation.lists ||= []
+
+    // This bit isn’t working yet so faked for now.
+    currentOrganisation.lists.push({
+      siteCode: "TODO"
+    })
+
+    res.redirect('/lists/list')
+
+  })
 
 }
