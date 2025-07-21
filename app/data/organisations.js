01f275def19a6ffda23a8005862bb247f1effe44
Frankie Roberto <frankie@frankieroberto.com>
2025-07-21 12:13:40 +0100

    Fix capitalisation

--- app/data/organisations.js
+++ app/data/organisations.js
@@ -716,7 +716,7 @@ module.exports = [
       {name: "COVID-19", status: "enabled"},
       {name: "pertussis", status: "disabled"},
       {name: "flu", status: "enabled"},
-      {name: "flu (London Flu service)", status: "enabled"}
+      {name: "flu (London flu service)", status: "enabled"}
     ],
     sites: [
       {
