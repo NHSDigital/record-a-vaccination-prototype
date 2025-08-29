a2775767e54f9bcbce1acbd66f898d530c83a56f
Frankie Roberto <frankie@frankieroberto.com>
2025-08-29 16:53:51 +0100

    Refactor and organise the code

--- app/routes/home.js
+++ app/routes/home.js
@@ -5,16 +5,17 @@ module.exports = router => {
     const data = req.session.data
     const vaccinationsRecorded = data.vaccinationsRecorded
 
+
+    let totalsBySite = []
+    let totalsByVaccine = []
+    let totalsByDay = []
+
     const dateToday = new Date()
-
-
     const dayToday = (dateToday.getDate())
 
     // Months in JavaScript are zero-indexed
     const monthToday = (dateToday.getMonth() + 1)
     const yearToday = (dateToday.getFullYear())
-
-    const stats = []
 
     const totalVaccinationsRecorded = vaccinationsRecorded.length
 
@@ -51,35 +52,20 @@ module.exports = router => {
       .filter((vaccine) => vaccine.status === "enabled")
       .map((vaccine) => vaccine.name)
 
+    for (let i = 0; i < 7; i++) {
+      totalsByDay.push({
+        day: dateToday.toISOString()
+      })
+    }
+
+
     let vaccines = vaccinesEnabledNames
 
     for (vaccine of vaccines) {
-
-      let vaccineStat = {
-        vaccine: vaccine,
-        days: []
-      }
-
-      for (let i = -1; i < 8; i++) {
-
-        let day = new Date(dateToday)
-        day.setDate(day.getDate() - i)
-
-        let count = Math.round(Math.random() * 20) + 1
-        if (i > 0) {
-          count += Math.round(Math.random() * 150)
-        }
-
-        vaccineStat.days.push({
-          date: day.toISOString().substring(0,10),
-          count: count
-        })
-      }
-
-      stats.push(vaccineStat)
+      totalsByVaccine.push({
+        vaccine: vaccine
+      })
     }
-
-    let totalsBySite = []
 
     // This is faked for now
     let siteIds = [...new Set(data.vaccineStock.map((vaccine) => vaccine.siteId))]
@@ -92,60 +78,14 @@ module.exports = router => {
 
 
     res.render('home/index', {
-      stats,
       totalVaccinationsRecorded,
       totalVaccinationsRecordedToday,
       totalVaccinationsRecordedThisMonth,
       totalVaccinationsRecordedPast7Days,
       monthToday,
-      totalsBySite
-    })
-  })
-
-  // Site-specific dashboard
-  router.get('/home/:siteId', (req, res) => {
-
-    const siteId = req.params.siteId
-    const site = req.session.data.sites[siteId]
-    const dateToday = new Date()
-    const stats = []
-
-    let vaccines = ["RSV", "Pertussis"]
-
-    if (Math.random() > 0.5) { vaccines.push("Flu") }
-    if (Math.random() > 0.5) { vaccines.push("Covid") }
-
-    for (vaccine of vaccines) {
-
-      let vaccineStat = {
-        vaccine: vaccine,
-        days: []
-      }
-
-      for (let i = -1; i < 8; i++) {
-
-        let day = new Date(dateToday)
-        day.setDate(day.getDate() - i)
-
-        let count = Math.round(Math.random() * 5) + 1
-        if (i > 0) {
-          count += Math.round(Math.random() * 30)
-        }
-
-        vaccineStat.days.push({
-          date: day.toISOString().substring(0,10),
-          count: count
-        })
-      }
-
-      stats.push(vaccineStat)
-    }
-
-
-    res.render('home/site', {
-      site,
-      siteId,
-      stats
+      totalsBySite,
+      totalsByVaccine,
+      totalsByDay
     })
   })
 }
