bc7b0c7211cf54932f92b3e40e69e9241e7c6793
Frankie Roberto <frankie@frankieroberto.com>
2025-09-01 14:14:34 +0100

    Refactor

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,56 +1,80 @@
 module.exports = router => {
 
+  const countVaccinations = function(allVaccinations, filters = {}) {
+
+    let vaccinations = JSON.parse(JSON.stringify(allVaccinations))
+
+    if (filters.date) {
+      vaccinations = vaccinations.filter((vaccination) => {
+        return (
+          parseInt(vaccination.date.day) == filters.date.getDate() &&
+          parseInt(vaccination.date.month) == (filters.date.getMonth() + 1) &&
+          parseInt(vaccination.date.year) == filters.date.getFullYear()
+        )
+      })
+    }
+
+    if (filters.month) {
+      vaccinations = vaccinations.filter((vaccination) => {
+        return (
+          parseInt(vaccination.date.month) == (filters.month.getMonth() + 1) &&
+          parseInt(vaccination.date.year) == filters.month.getFullYear()
+        )
+      })
+    }
+
+    if (filters.minDate && filters.maxDate) {
+      vaccinations = vaccinations.filter((vaccination) => {
+
+        const vaccinationYear = parseInt(vaccination.date.year)
+        const vaccinationMonth = parseInt(vaccination.date.month)
+        const vaccinationDay = parseInt(vaccination.date.day)
+        const vaccinationDate = new Date(Date.UTC(vaccinationYear, (vaccinationMonth - 1), vaccinationDay, 0, 0, 0));
+
+        return (
+          vaccinationDate.getTime() > filters.minDate.getTime() &&
+          vaccinationDate.getTime() <= (filters.maxDate.getTime())
+        )
+      })
+    }
+
+    return vaccinations.length
+  }
+
   // Dashboard
   router.get('/home', (req, res) => {
     const data = req.session.data
     const vaccinationsRecorded = data.vaccinationsRecorded
+    const dateToday = new Date()
+    const monthToday = (dateToday.getMonth() + 1) // JavaScript dates are 0-indexed
 
     let totalsBySite = []
     let totalsByVaccine = []
     let totalsByDay = []
 
-    const dateToday = new Date()
-    const dayToday = (dateToday.getDate())
+    const totalVaccinationsRecorded = countVaccinations(vaccinationsRecorded)
 
-    // Months in JavaScript are zero-indexed
-    const monthToday = (dateToday.getMonth() + 1)
-    const yearToday = (dateToday.getFullYear())
+    const totalVaccinationsRecordedToday = countVaccinations(
+      vaccinationsRecorded,
+      {date: dateToday}
+    )
 
-    const totalVaccinationsRecorded = vaccinationsRecorded.length
+    const sevenDaysAgo = new Date(Date.UTC(dateToday.getFullYear(), dateToday.getMonth(), dateToday.getDate() - 7, 0, 0, 0));
 
-    const totalVaccinationsRecordedToday = vaccinationsRecorded.filter((vaccination) => {
-      return (
-        parseInt(vaccination.date.day) == dayToday &&
-        parseInt(vaccination.date.month) == monthToday &&
-        parseInt(vaccination.date.year) == yearToday
-      )
-    }).length
+    const totalVaccinationsRecordedPast7Days = countVaccinations(
+      vaccinationsRecorded,
+      {minDate: sevenDaysAgo, maxDate: dateToday}
+    )
 
-    const totalVaccinationsRecordedPast7Days = vaccinationsRecorded.filter((vaccination) => {
-
-      const vaccinationYear = parseInt(vaccination.date.year)
-      const vaccinationMonth = parseInt(vaccination.date.month)
-      const vaccinationDay = parseInt(vaccination.date.day)
-      const vaccinationDate = new Date(Date.UTC(vaccinationYear, (vaccinationMonth - 1), vaccinationDay, 0, 0, 0));
-
-      const daysAgo = Math.floor((dateToday - vaccinationDate) / (1000*60*60*24))
-
-      return (daysAgo < 8)
-    }).length
-
-    const totalVaccinationsRecordedThisMonth = vaccinationsRecorded.filter((vaccination) => {
-      return (
-        parseInt(vaccination.date.month) == monthToday &&
-        parseInt(vaccination.date.year) == yearToday
-      )
-    }).length
+    const totalVaccinationsRecordedThisMonth = countVaccinations(vaccinationsRecorded, {
+      month: dateToday
+    })
 
     const organisationVaccines = res.locals.currentOrganisation.vaccines || []
 
     const vaccinesEnabledNames = organisationVaccines
       .filter((vaccine) => vaccine.status === "enabled")
       .map((vaccine) => vaccine.name)
-
 
 
     for (let i = 0; i < 7; i++) {
@@ -60,14 +84,9 @@ module.exports = router => {
 
       totalsByDay.push({
         day: date.toISOString(),
-        value: vaccinationsRecorded.filter((vaccination) => {
-          return (
-            parseInt(vaccination.date.day) == date.getDate() &&
-            parseInt(vaccination.date.month) == (date.getMonth() + 1) &&
-            parseInt(vaccination.date.year) == date.getFullYear()
-          )
-        }).length
-
+        value: countVaccinations(vaccinationsRecorded, {
+          date: date
+        })
       })
     }
 
