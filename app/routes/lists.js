18cba99bc7dae3ba8dfa4e1a358c03e826f6df47
johnrimmer2 <john.rimmer2@nhs.net>
2025-10-06 12:56:59 +0100

    changes

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -1,29 +1,19 @@
 module.exports = router => {
+ 
 
+router.post('/lists/date-answer', (req, res) => {
 
-  router.get('/lists', (req, res) => {
-
-    const currentOrganisation = res.locals.currentOrganisation;
-    const lists = currentOrganisation.lists || []
-
-    res.render('lists/index', {
-      lists
-    })
-
-  })
-
-  router.post('/lists/add-numbers', (req, res) => {
-
-    const currentOrganisation = res.locals.currentOrganisation;
-    currentOrganisation.lists ||= []
-
-    // This bit isn’t working yet so faked for now.
-    currentOrganisation.lists.push({
-      siteCode: "TODO"
-    })
-
-    res.redirect('/lists/list')
-
+	const date = req.session.data.date
+  
+	if (date === 'no-date') {
+  
+	  res.redirect('/lists/name')
+  
+	} else  {
+  
+	  res.redirect('/lists/add-nhs-numbers')
+  
+	} 
   })
 
 }
