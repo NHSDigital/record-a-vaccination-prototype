10032f41dd73f7bf67a9e81be90a0cf6b829596b
Frankie Roberto <frankie.roberto1@nhs.net>
2025-10-21 13:58:07 +0100

    Add legal mechanism to Record vaccinations and Records sections (#495)
    This adds legal mechanism into the streamlined journey.
    
    It also adds legal mechanisms to the vaccines section as checkbox option
    you can select. This then reduces the number of options shown when
    recording a vaccination, or if you only select a single option then it
    skips the question entirely.
    
    
    ## Record vaccinations
    
    <img width="761" height="552" alt="Screenshot 2025-10-21 at 13 42 49"
    src="https://github.com/user-attachments/assets/476f95ed-dedb-41b8-88fa-c26bcf3f4346"
    />
    
    ## Check answers summary
    
    <img width="782" height="698" alt="Screenshot 2025-10-21 at 13 43 31"
    src="https://github.com/user-attachments/assets/fbdb5785-62a6-405c-a62a-2a2046b51f2b"
    />
    
    ## Records section
    
    <img width="692" height="695" alt="Screenshot 2025-10-21 at 13 57 09"
    src="https://github.com/user-attachments/assets/35cfa19c-820d-4778-a155-eba3fb69e2b6"
    />
    
    ---------
    
    Co-authored-by: Anna-Sutton <anna.sutton9@nhs.net>

--- app/routes/helpers.js
+++ app/routes/helpers.js
@@ -6,6 +6,8 @@ module.exports = router => {
     const data = req.session.data
     const currentOrganisation = res.locals.currentOrganisation
 
+    const allLegalMechanisms = data.legalMechanisms.map((legalMechaism) => legalMechaism.value)
+
     let vaccineStock = data.vaccineStock
 
     const organisationVaccines = currentOrganisation.vaccines
@@ -55,6 +57,7 @@ module.exports = router => {
           id: generatedVaccineId,
           vaccine: vaccine.name,
           vaccineProduct: vaccineProduct.name,
+          legalMechanisms: allLegalMechanisms,
           siteId: siteId,
           batches: batches
         })
@@ -103,6 +106,8 @@ module.exports = router => {
       const randomVaccineProduct = randomItem(vaccineProductsInStock)
       const randombatchNumber = randomItem(randomVaccineProduct.batches)
 
+      const randomLegalMechanism = randomItem(data.legalMechanisms).value
+
       const vaccinator = randomItem(vaccinators)
 
       const randomName = randomItem(listOfFirstNames) + " " + randomItem(listOfLastNames)
@@ -127,6 +132,7 @@ module.exports = router => {
         batchNumber: randombatchNumber.batchNumber,
         batchExpiryDate: "2025-01-05",
         vaccinatorId: vaccinator.id,
+        legalMechanism: randomLegalMechanism,
         eligibility: ["Pregnant"],
         pregnancyDueDate: {
           day: "04",
