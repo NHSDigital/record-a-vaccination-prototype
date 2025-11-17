157a485a23c7289e19b73a89b4b69baf0c87ed26
Frankie Roberto <frankie@frankieroberto.com>
2025-11-17 15:50:11 +0000

    Redirect to sign in if needed

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,23 @@ const express = require('express');
 
 const router = express.Router();
 
+const authorise = function(req, res, next) {
+  if (!req.session.data.currentUserId) {
+    res.redirect('/auth/okta-sign-in')
+  } else {
+    next()
+  }
+}
+
+// These routes require being logged in
+router.use('/home{*splat}', authorise)
+router.use('/record-vaccinations{*splat}', authorise)
+router.use('/vaccines{*splat}', authorise)
+router.use('/records{*splat}', authorise)
+router.use('/reports{*splat}', authorise)
+router.use('/user-admin{*splat}', authorise)
+router.use('/user-profile{*splat}', authorise)
+
 require('./routes/apply')(router)
 require('./routes/record-vaccinations')(router)
 require('./routes/regions')(router)
