7f07b321b3bf3baae645a5a4b26029ae3276664c
Frankie Roberto <frankie@frankieroberto.com>
2025-11-30 19:58:56 +0000

    Fix for locals.js

--- app/locals.js
+++ app/locals.js
@@ -1,6 +1,4 @@
 module.exports = function(req, res, next) {
-  // Set currentUser and currentOrganisation for convenience
-  res.locals.currentUser = req.session.data.users.find((user) => user.id === req.session.data.currentUserId);
 
     // Set currentUser for convenience
     if (req.session.data.currentUserId) {
@@ -8,7 +6,6 @@ module.exports = function(req, res, next) {
     } else {
       res.locals.currentUser = null
     }
-
     // Set currentOrganisation for convenience
     if (req.session.data.currentOrganisationId) {
       res.locals.currentOrganisation = req.session.data.organisations.find((organisation) => organisation.id === req.session.data.currentOrganisationId)
@@ -17,7 +14,7 @@ module.exports = function(req, res, next) {
     }
 
   // set current logged in region
-  res.locals.currentRegion = req.session.data.regions.find((region) => region.id === req.session.data.currentRegionId);
+  // res.locals.currentRegion = req.session.data.regions.find((region) => region.id === req.session.data.currentRegionId);
 
   next()
 }
