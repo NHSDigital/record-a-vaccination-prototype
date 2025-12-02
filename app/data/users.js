b2da4568a35ec649f7141fb0530b807c826859ae
Frankie Roberto <frankie.roberto1@nhs.net>
2025-12-02 23:19:57 +0000

    Reporting across organisations (#556)
    This piggy-backs on the multi-org user functionality to enable a new
    mode for users who are administrators in multiple organisations.
    
    They'll see this screen when logging in:
    
    <img width="1011" height="695" alt="Screenshot 2025-11-26 at 15 15 45"
    src="https://github.com/user-attachments/assets/bb17bf8e-6828-4254-9e4f-60f67ed98cef"
    />
    
    If they select the second option they'll see a reduced navigation which
    only has 'Home' and 'Reports':
    
    <img width="994" height="246" alt="Screenshot 2025-11-26 at 15 15 57"
    src="https://github.com/user-attachments/assets/e102b5bb-d89b-47a7-8669-73b01198637b"
    />
    
    Within 'Reports', they'll then be able to export a single report across
    organisations/pharmacies:
    
    <img width="1096" height="1064" alt="Screenshot 2025-11-28 at 14 40 02"
    src="https://github.com/user-attachments/assets/7a0066af-9b31-4366-8c29-8171f6e1241a"
    />
    
    If on the first screen they select 'Use the service at 1 organisation',
    they can pick the organisation, and will log in as that organisation
    only. A Back link lets them to back to change their answer.
    
    <img width="1005" height="651" alt="Screenshot 2025-12-02 at 23 13 54"
    src="https://github.com/user-attachments/assets/3f2de339-aef3-4fb9-980b-c3091dc3f168"
    />
    
    When logging in as a single organisation, the organisation name is now
    shown in the header.
    
    <img width="997" height="166" alt="Screenshot 2025-12-02 at 23 15 31"
    src="https://github.com/user-attachments/assets/8cba73af-993c-48f3-9520-26ec23d1baed"
    />
    
    This will be truncated if the name is very long:
    
    <img width="993" height="151" alt="Screenshot 2025-12-02 at 23 16 36"
    src="https://github.com/user-attachments/assets/344d27e7-40de-4ffa-9d95-71152c4eeca5"
    />

--- app/data/users.js
+++ app/data/users.js
@@ -15,6 +15,23 @@ module.exports = [
     "firstName": "Jane",
     "lastName": "Smith"
   },
+  // Henry is a lead admin for an NHS trust
+  // that has only just been added (no vaccines,
+  // users or vaccinations)
+  {
+    "id": "16346346361",
+    "email": "henry.blue@nhs.net",
+    "organisations": [
+      {
+        "id": "RV3",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": true
+      }
+    ],
+    "firstName": "Henry",
+    "lastName": "Blue"
+  },
   // Graham Wallace is a recorder at
   // multiple NHS Trusts (eg bank staff)
   {
@@ -22,7 +39,7 @@ module.exports = [
     "email": "graham.wallace@nhs.net",
     "organisations": [
       {
-        "id": "RLR",
+        "id": "RWP",
         "permissionLevel": "Recorder",
         "status": "Active",
         "vaccinator": true
@@ -42,6 +59,28 @@ module.exports = [
     ],
     "firstName": "Graham",
     "lastName": "Wallace"
+  },
+  // Phoebe Black is a recorder at
+  // multiple NHS Trusts (eg bank staff)
+  {
+    "id": "2058253531",
+    "email": "phoebe.black@nhs.net",
+    "organisations": [
+      {
+        "id": "RWP",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "RXX",
+        "permissionLevel": "Administrator",
+        "status": "Active",
+        "vaccinator": false
+      }
+    ],
+    "firstName": "Phoebe",
+    "lastName": "Black"
   },
   // Paulina Sloan is a lead admin for
   // a chain of pharmacies
@@ -76,6 +115,122 @@ module.exports = [
     ],
     "firstName": "Paulina",
     "lastName": "Sloan"
+  },
+  // Each pharmacy in Paulina Sloan’s chain has its own vaccinator
+  {
+    "id": "46436346",
+    "email": "jeremy.blue@nhs.net",
+    "organisations": [
+      {
+        "id": "FA424",
+        "permissionLevel": "Recorder",
+        "status": "Active",
+        "vaccinator": true
+      }
+    ],
+    "firstName": "Jeremy",
+    "lastName": "Blue"
+  },
+
+  {
+    "id": "64746353",
+    "email": "jeremy.blue@nhs.net",
+    "organisations": [
+      {
+        "id": "FA02S",
+        "permissionLevel": "Recorder",
+        "status": "Active",
+        "vaccinator": true
+      }
+    ],
+    "firstName": "Jeremy",
+    "lastName": "Blue"
+  },
+  {
+    "id": "46436436436",
+    "email": "jeremy.blue@nhs.net",
+    "organisations": [
+      {
+        "id": "FVJ99",
+        "permissionLevel": "Recorder",
+        "status": "Active",
+        "vaccinator": true
+      }
+    ],
+    "firstName": "Jeremy",
+    "lastName": "Blue"
+  },
+  {
+    "id": "646436311",
+    "email": "jeremy.blue@nhs.net",
+    "organisations": [
+      {
+        "id": "PDL93",
+        "permissionLevel": "Recorder",
+        "status": "Active",
+        "vaccinator": true
+      }
+    ],
+    "firstName": "Jeremy",
+    "lastName": "Blue"
+  },
+  // Amanda White is a lead admin for
+  // a chain of pharmacies
+  {
+    "firstName": "Amanda",
+    "lastName": "White",
+    "id": "6424325235325",
+    "email": "amanda.white@nhs.net",
+    "organisations": [
+      {
+        "id": "PX9141",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "PX4825",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "PX7314",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "PX9151",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "PX2525",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "PC8141",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "P9824",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      },
+      {
+        "id": "PD812",
+        "permissionLevel": "Lead administrator",
+        "status": "Active",
+        "vaccinator": false
+      }
+    ]
   },
   {
     "id": "1394978032564",
