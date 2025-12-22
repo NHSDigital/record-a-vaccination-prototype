e1280e652dcf4ff5cc703b3ad0622d98f356abda
Frankie Roberto <frankie@frankieroberto.com>
2025-12-22 13:55:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const express = require('express')
 const NHSPrototypeKit = require('nhsuk-prototype-kit')
+
 const path = require('path')
-const nunjucks = require('nunjucks')
 const { join } = require('node:path')
 
 // Local dependencies
@@ -16,48 +15,21 @@ const SERVICE_NAME = config.serviceName
 // Set configuration variables
 const port = parseInt(process.env.PORT, 10) || 2000
 
-// Initialise applications
-const app = express()
-
-// Add variables that are available in all views
-app.locals.asset_path = '/public/'
-
-// Nunjucks configuration for application
-const appViews = [
-  join(__dirname, 'app/views/'),
-  join(__dirname, 'app/components/'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk/components'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk/macros'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk'),
-  join(__dirname, 'node_modules/nhsuk-frontend/dist')
-]
-
-let nunjucksAppEnv = nunjucks.configure(appViews, { express: app, noCache: true })
-
-for (const [name, filter] of Object.entries(filters())) {
-  nunjucksAppEnv.addFilter(name, filter)
-}
-
-
-// Use public folder for static assets
-app.use(express.static(join(__dirname, 'public')))
-
-// Use assets from NHS frontend
-app.use(
-  '/nhsuk-frontend',
-  express.static(join(__dirname, 'node_modules/nhsuk-frontend/dist/nhsuk'))
-)
+const viewsPath = join(__dirname, 'app/views/')
 
 const prototype = NHSPrototypeKit.init({
   serviceName: SERVICE_NAME,
-  express: app,
-  nunjucks: nunjucksAppEnv,
   routes: routes,
   locals: locals,
   sessionDataDefaults: sessionDataDefaults,
+  viewsPath: viewsPath,
   buildOptions: {
     entryPoints: ['app/assets/sass/main.scss']
   }
 })
+
+for (const [name, filter] of Object.entries(filters())) {
+  prototype.nunjucks.addFilter(name, filter)
+}
 
 prototype.start()
