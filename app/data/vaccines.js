bc33165faee0469ac24d112d04ef825cfc5dc694
Frankie Roberto <frankie@frankieroberto.com>
2026-02-02 13:23:13 +0000

    Add full list of known possible vaccines
    This adds all the vaccines given by the NHS that we currently know about it.
    
    The aim is to stress test the interface to see how it’d handle having longer lists of vaccines and vaccine products, and to see what might need to be changed.

--- app/data/vaccines.js
+++ app/data/vaccines.js
@@ -129,5 +129,132 @@ module.exports = [
         type: "injection"
       }
     ]
+  },
+  {
+    name: "BCG",
+    products: [
+      {
+        name: "BCG Vaccine AJV",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "MMRV",
+    products: [
+      {
+        name: "Priorix-Tetra",
+        type: "injection"
+      },
+      {
+        name: "ProQuad",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "HPV",
+    products: [
+      {
+        name: "Gardasil 9 ",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "Hep B",
+    products: [
+      {
+        name: "Engerix B",
+        type: "injection"
+      },
+      {
+        name: "Fendrix",
+        type: "injection"
+      },
+      {
+        name: "HBvaxPRO",
+        type: "injection"
+      },
+      {
+        name: "Twinrix",
+        type: "injection"
+      },
+      {
+        name: "Ambirix",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "MenB",
+    products: [
+      {
+        name: "Bexsero",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "6-in-1 (DTaP/IPV/Hib/HepB)",
+    products: [
+      {
+        name: "Vaxelis",
+        type: "injection"
+      },
+      {
+        name: "Infanrix Hexa",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "4-in-1 (dTaP/IPV)",
+    products: [
+      {
+        name: "Boostrix-IPV",
+        type: "injection"
+      },
+      {
+        name: "Repevax",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "3-in-1 teenage booster (Td/IPV)",
+    products: [
+      {
+        name: "Revaxis",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "MenACWY",
+    products: [
+      {
+        name: "MenQuadfi",
+        type: "injection"
+      }
+    ]
+  },
+  {
+    name: "Rotavirus",
+    products: [
+      {
+        name: "Rotarix",
+        type: "oral"
+      }
+    ]
+  },
+  {
+    name: "Shingles",
+    products: [
+      {
+        name: "Shingrix",
+        type: "injection"
+      }
+    ]
   }
 ]
