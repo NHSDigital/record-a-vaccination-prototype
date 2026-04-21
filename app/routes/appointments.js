2058ad4d41b6545f70a9953e018b35ec9c1c8b15
Frankie Roberto <frankie@frankieroberto.com>
2026-04-21 22:15:58 +0100

    Make date navigation work

--- app/routes/appointments.js
+++ app/routes/appointments.js
@@ -11,10 +11,22 @@ module.exports = (router) => {
 
     const completedAppointments = appointments.filter((appointment) => (appointment.vaccinationIds || []).length > 0)
 
+    const date = (req.query.date ? (new Date(req.query.date)) : (new Date()))
+    const day = 86400000 // number of milliseconds in a day
+    const previousDay = (new Date(date.getTime() - day)).toISOString().substring(0,10)
+    const nextDay = (new Date(date.getTime() + day)).toISOString().substring(0,10)
+    const currentDay = date.toISOString().substring(0,10)
+    const today = (new Date()).toISOString().substring(0,10)
+
+
     res.render('appointments/index', {
       scheduledAppointments,
       cancelledAppointments,
-      completedAppointments
+      completedAppointments,
+      today,
+      currentDay,
+      previousDay,
+      nextDay
     })
   })
 }
