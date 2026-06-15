4d79c2481daf6b166907416f0d60abc32d16af4a
Caitlin Roach <caitlin.roach1@nhs.net>
2026-06-15 14:49:11 +0100

    Revert "added some deactivated users bydefault"
    This reverts commit 15d8fadec32941d9ffb00f67e6f4acf15f4562ee.

--- app/routes/pharmacies.js
+++ app/routes/pharmacies.js
@@ -528,12 +528,10 @@ module.exports = router => {
 
     const user = data.users.find(user => user.id === userId)
     const pharmacy = data.organisations.find(organisation => organisation.id === pharmacyId)
-    const currentUser = data.users.find(u => u.id === data.currentUserId)
 
     const role = user.organisations.find(role => role.id === pharmacyId)
 
     role.status = 'Deactivated'
-    role.deactivatedBy = currentUser ? `${currentUser.firstName} ${currentUser.lastName}` : 'Unknown'
 
     if (from === 'user') {
       return res.redirect(`/pharmacies/users/${user.id}?deactivatedFromPharmacyId=${pharmacy.id}`)
@@ -541,7 +539,7 @@ module.exports = router => {
 
     res.redirect(`/pharmacies/${pharmacy.id}?tab=deactivated&deactivatedUserId=${user.id}&deactivatedFromPharmacyId=${pharmacy.id}`)
 
-  }))
+  })
 
   router.get('/pharmacies/:pharmacyId/users/:userId/reactivate', (req, res) => {
     const data = req.session.data
@@ -600,7 +598,6 @@ module.exports = router => {
     const companyId = res.locals.currentOrganisation.id
 
     const user = data.users.find((item) => item.id === userId)
-    const currentUser = data.users.find(u => u.id === data.currentUserId)
 
     if (!user) {
       return res.redirect('/pharmacies/users')
@@ -613,7 +610,6 @@ module.exports = router => {
 
       if (pharmacy && pharmacy.companyId === companyId && role.permissionLevel !== 'Group administrator' && role.status !== 'Deactivated') {
         role.status = 'Deactivated'
-        role.deactivatedBy = currentUser ? `${currentUser.firstName} ${currentUser.lastName}` : 'Unknown'
         deactivatedPharmacyIds.push(pharmacy.id)
       }
     }
